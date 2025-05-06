7c21b0c8c96f3c2a714cbf1f889c1d5473dfbe83
Gilberto Silveira <gilbertosilveira@192.168.1.103>
2025-05-06 15:02:33 +0100

    feat: Implement RSSFeed model to pass initial test.

--- feeds/models.py
+++ feeds/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class RSSFeed(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="feeds")
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
