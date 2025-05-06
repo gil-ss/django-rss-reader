d29fc43d21350e532fbd3f80a1779aa3989d83ee
Gilberto Silveira <gilbertosilveira@192.168.1.103>
2025-05-06 15:09:12 +0100

    feat: Add RSSItem model and pass creation test.

--- feeds/models.py
+++ feeds/models.py
@@ -6,6 +6,14 @@ User = get_user_model()
 class RSSFeed(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="feeds")
     url = models.URLField()
-
     def __str__(self):
         return self.url
+
+
+class RSSItem(models.Model):
+    feed = models.ForeignKey(RSSFeed, on_delete=models.CASCADE, related_name="items")
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    pub_date = models.DateTimeField()
+    def __str__(self):
+        return self.title
--- feeds/tests/test_models.py
+++ feeds/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from feeds.models import RSSFeed
+from datetime import datetime, timezone
 
 User = get_user_model()
 
@@ -9,6 +10,20 @@ class RSSFeedModelTest(TestCase):
     def test_create_rss_feed(self):
         user = User.objects.create_user(username="john", password="secret")
         feed = RSSFeed.objects.create(user=user, url="https://example.com/rss.xml")
-
         self.assertEqual(feed.url, "https://example.com/rss.xml")
         self.assertEqual(feed.user.username, "john")
+
+
+class RSSItemModelTest(TestCase):
+    def test_create_rss_item(self):
+        user = User.objects.create_user(username="john", password="secret")
+        feed = RSSFeed.objects.create(user=user, url="https://example.com/rss.xml")
+        pub_date = datetime(2025, 5, 5, 12, 0, 0, tzinfo=timezone.utc)
+        item = feed.items.create(
+            title="Example News",
+            description="Example Description",
+            pub_date=pub_date
+        )
+        self.assertEqual(item.title, "Example News")
+        self.assertEqual(item.description, "Example Description")
+        self.assertEqual(item.pub_date, pub_date)
