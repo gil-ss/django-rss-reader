cd634f13f4191c03ad9d4d520407a2326cf36d2c
Gilberto Silveira <gilbertosilveira@192.168.1.103>
2025-05-07 08:36:45 +0100

    test(views): Add tests for FeedCreateView including access control and feed creation.

--- feeds/tests/test_views.py
+++ feeds/tests/test_views.py
@@ -43,3 +43,27 @@ class FeedDetailViewTest(TestCase):
         url = reverse("feed-detail", kwargs={"pk": self.other_feed.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+
+class FeedCreateViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="john", password="secret")
+        self.client.login(username="john", password="secret")
+
+    def test_feed_create_view_get(self):
+        response = self.client.get(reverse("feed-add"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "feeds/feed_form.html")
+
+    def test_feed_create_view_post_creates_feed(self):
+        form_data = {"url": "http://example.com/rss"}
+        response = self.client.post(reverse("feed-add"), data=form_data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(RSSFeed.objects.count(), 1)
+        self.assertEqual(RSSFeed.objects.first().user, self.user)
+
+    def test_feed_create_requires_authentication(self):
+        self.client.logout()
+        response = self.client.get(reverse("feed-add"))
+        self.assertRedirects(response, "/login/?next=/add/")
